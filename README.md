38ece0725755d650019a089c222d4daa91812db3
MnyaCat <mnnk.1314@gmail.com>
2024-02-27 04:38:02 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# SSH Notify
+
+SSHのログイン/ログアウト通知をDiscordに送信するためのPythonスクリプトです。
+
+## Requirements
+
+```
+python: 3.12.2
+certifi==2024.2.2
+charset-normalizer==3.3.2
+idna==3.6
+netifaces==0.11.0
+requests==2.31.0
+urllib3==2.2.1
+```
+
+## Usage
+
+1. `$ python -m venv .env`
+2. `$ pip install -r requirements.txt`
+3. discord-notify.pyを実行するシェルスクリプトを作成(以下は例)
+
+```bash
+#!/bin/bash
+
+cd /home/username/ssh-notify
+source .env/bin/activate
+python notify_discord.py
+```
+
+4. `/etc/pam.d/sshd`に以下を追加
+
+```
+session optional pam_exec.so type=open_session /home/username/ssh-notify/notify_discord.sh
+```
+
+## Reference
+
+[UbuntuへのSSHログインをDiscordに通知してみる](https://zenn.dev/myuki/articles/4324ab701fe5ab)
+
+## Licence
+
+Copyright (c) 2024 MnyaCat  
+Released under the MIT license  
+see [LICENSE file](./LICENSE)
---
+++ notify_discord.py
@@ -0,0 +1,64 @@
+import requests
+import socket
+import os
+import datetime
+import netifaces as ni
+
+
+with open("webhook_url.txt") as f:
+    webhook_url = f.read()
+header = "Content-Type: application/json"
+
+
+def getIPaddress(nface: str) -> str:
+    """
+    Get IPv4 Address of NIC.
+
+    Args:
+        nface str: NIC Name
+
+    Returns:
+        IP Address v4
+    """
+
+    return ni.ifaddresses(nface)[ni.AF_INET][0]["addr"]
+
+
+def main():
+
+    pam_type = os.getenv("PAM_TYPE")
+    hostname = socket.gethostname()
+    bot_usernm = f"{hostname} notification"
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M%S%z")
+
+    if (pam_type == "open_session"):
+        data = {
+            "username": bot_usernm,
+            "embeds": [
+                {
+                    "title": "Login notification",
+                    "description": f'{os.getenv("PAM_USER")} logged in to {hostname} from {os.getenv("PAM_RHOST")}'
+                    f'({getIPaddress("en01")}, {getIPaddress("ts01")})',
+                    "color": 0x248046,
+                    "timestamp": timestamp
+                },
+            ],
+        }
+    elif (pam_type == "close_session"):
+        data = {
+            "username": bot_usernm,
+            "embeds": [
+                {
+                    "title": "Logout notification",
+                    "description": f'{os.getenv("PAM_USER")} logged out to {hostname} from {os.getenv("PAM_RHOST")}'
+                    f'({getIPaddress("en01")}, {getIPaddress("ts01")})',
+                    "timestamp": timestamp
+                },
+            ],
+        }
+
+    requests.post(webhook_url, json=data)
+
+
+if __name__ == "__main__":
+    main()
